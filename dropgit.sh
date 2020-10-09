e8963d975c60928baa2d359c0162b7a10063596f
Mohammad <moh.ebrahimi2000@gmail.com>
2020-10-09 18:13:51 +0330

    Add git not install error handling

--- dropgit.sh
+++ dropgit.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+# Check if git is not installed on your system
+if ! [ -x "$(command -v git)" ]; then
+	echo "Dropgit Error: You don't have git on your system. Please install it first" >&2
+	exit 1
+fi
+
 BASE_DIR="$(basename `git rev-parse --show-toplevel`)"
-
 git init --bare ~/Dropbox/git/$BASE_DIR.git
-
 git remote add origin ~/Dropbox/git/$BASE_DIR.git
-
