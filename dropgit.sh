4330fbcd7afac5ae9e6cca664555d75d076e4945
Mohammad <moh.ebrahimi2000@gmail.com>
2020-10-09 18:16:28 +0330

    Add dropbox not install error handling

--- dropgit.sh
+++ dropgit.sh
@@ -6,6 +6,12 @@ if ! [ -x "$(command -v git)" ]; then
 	exit 1
 fi
 
+# Check if dropbox in not installed on your system
+if ! [ -x "$(command -v dropbox)" ]; then
+	echo "Dropgit Error: You don't have dropbox on your system. Please install it first" >&2
+	exit 2
+fi
+
 BASE_DIR="$(basename `git rev-parse --show-toplevel`)"
 git init --bare ~/Dropbox/git/$BASE_DIR.git
 git remote add origin ~/Dropbox/git/$BASE_DIR.git
